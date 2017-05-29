d8bc2eb93759405c782a5443933eba62be3523be
nicolac93 <nicola.cortesi93@gmail>
2017-05-29 13:24:20 +0200

    add html files

---
+++ js/map.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoibmljb2xhOTMiLCJhIjoiY2l2Y2ozYnZ5MDBocTJ5bzZiM284NGkyMiJ9.4VUvTxBv0zqgjY7t3JTFOQ';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/dark-v9',
+    center: [9.13734351262877, 45.137451890638886],
+    zoom: 6
+});
