a8f5c8da0b4aec6392e2fb5f3d43ae64af3997f3
Black4Key <kevin.got10@gmail.com>
2017-05-30 17:53:25 +0200

    integrate le funzioni con gli input da utente

--- treets/main.py
+++ treets/main.py
@@ -1,40 +1,52 @@
-import flask
-import flask.views
+from flask import Flask, render_template, request
 from pymongo import MongoClient
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
 MAPBOX_ACCESS_KEY = 'pk.eyJ1Ijoibmljb2xhOTMiLCJhIjoiY2l2Y2ozYnZ5MDBocTJ5bzZiM284NGkyMiJ9.4VUvTxBv0zqgjY7t3JTFOQ'
 app.debug = True
 
 
-class View(flask.views.MethodView):
-
-    def get(self):
-        return flask.render_template('index.html', ACCESS_KEY=MAPBOX_ACCESS_KEY)
-
-    def post(self):
-        return "Works!"
+@app.route('/', methods=['GET', 'POST'])		
+def main():
+	return render_template('index.html', ACCESS_KEY=MAPBOX_ACCESS_KEY)
 
 def show_all_tweets():
     import pdb; pdb.set_trace()
 
 @app.route('/searchText', methods=['GET', 'POST'])
 def searchText():
-    return "searching..."
+	'''
+	Search for the input string in the teets' text
+	'''
+	src = request.form['src']
+	message = src
+	if src == '':
+		message = 'campo mancante'
+	return message
 
 
 @app.route('/geo', methods=['GET', 'POST'])
 def geo():
-
-    return "cirlce O"
+	'''
+	Search every tweets inside the circle
+	center in gps location
+	radius given by user
+	'''
+	#TODO controllare che l'input sia numerico
+	lat = request.form['lat']
+	lon = request.form['lon']
+	radius = request.form['radius']
+	message = 'lat: '+ lat +' lon: '+ lon +' rad: '+ radius
+	if lat == '' or lon == '' or radius == '':
+		message = 'campo/i mancante'
+	return message
 
 
 @app.route('/export', methods=['GET', 'POST'])
 def export():
     return "exporting"
 
-app.add_url_rule('/', view_func=View.as_view('main'), methods=['GET', 'POST'])
 
 # TODO: put this function in a class
 def init():
