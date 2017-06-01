9b32e9ecdcc308afd28d68a4594a8443e10dd7b8
dnlcrl <ciriello.daniele@gmail.com>
2017-06-01 17:43:07 +0200

    Refactoring

--- treets/treets.py
+++ treets/treets.py
@@ -9,8 +9,8 @@ DEBUG_MODE = True
 class Application(flask.Flask):
     """docstring for ClassName"""
 
-    def __init__(self, import_name):
-        super(Application, self).__init__(import_name)
+    def __init__(self, args):
+        super(Application, self).__init__(*args)
 
         self.route('/', methods=['GET', 'POST'])(self.main)
         self.route('/searchText', methods=['GET', 'POST'])(self.search_text)
