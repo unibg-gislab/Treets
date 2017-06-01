b9574c95f6941f12a560391eccd9c9f7e64d197b
dnlcrl <ciriello.daniele@gmail.com>
2017-06-01 17:27:35 +0200

    Refactoring

--- treets/treets.py
+++ treets/treets.py
@@ -1,58 +1,60 @@
-from flask import Flask, render_template, request
+from flask# import Flask, render_template, request
 from pymongo import MongoClient
 
-app = Flask(__name__)
-app.debug = True
-MAPBOX_ACCESS_KEY = 'pk.eyJ1Ijoibmljb2xhOTMiLCJhIjoiY2l2Y2ozYnZ5MDBocTJ5bzZiM284NGkyMiJ9.4VUvTxBv0zqgjY7t3JTFOQ'
 
+class Application(flask.Flask):
+    """docstring for ClassName"""
 
-@app.route('/', methods=['GET', 'POST'])		
-def main():
-	return render_template('index.html', ACCESS_KEY=MAPBOX_ACCESS_KEY)
+    def __init__(self, import_name, mapbox_access_token, debug=False):
+        super(Application, self).__init__(import_name)
 
-def show_all_tweets():
-    import pdb; pdb.set_trace()
+        self.route('/', methods=['GET', 'POST'])(self.main)
+        self.route('/searchText', methods=['GET', 'POST'])(self.search_text)
+        self.route('/geo', methods=['GET', 'POST'])(self.search_within_cirle)
+        self.routeroute('/export', methods=['GET', 'POST'])(self.export)
 
-@app.route('/searchText', methods=['GET', 'POST'])
-def searchText():
-	'''
-	Search for the input string in the teets' text
-	'''
-	src = request.form['src']
-	message = src
-	if src == '':
-		message = 'campo mancante'
-	return message
+        self.mapbox_access_token = mapbox_access_token
+        self.mongo = MongoClient()
+        self.db = app.mongo.test
+        self.debug = debug
 
+    def run(self):
+        self.app.run()
 
-@app.route('/geo', methods=['GET', 'POST'])
-def geo():
-	'''
-	Search every tweets inside the circle
-	center in gps location
-	radius given by user
-	'''
-	#TODO controllare che l'input sia numerico
-	lat = request.form['lat']
-	lon = request.form['lon']
-	radius = request.form['radius']
-	message = 'lat: '+ lat +' lon: '+ lon +' rad: '+ radius
-	if lat == '' or lon == '' or radius == '':
-		message = 'campo/i mancante'
-	return message
+    def main(self):
+        return render_template('index.html', ACCESS_KEY=MAPBOX_ACCESS_KEY)
 
+    def show_all_tweets(self):
+        import pdb
+        pdb.set_trace()
 
-@app.route('/export', methods=['GET', 'POST'])
-def export():
-    return "exporting"
+    def search_text(self):
+        '''
+        Search for the input string in the tweets' text
+        '''
+        words = request.form['src']
+        if words:
+            tweets = query_issuer.search(words)
 
+    def search_within_cirle(self):
+        '''
+        Search every tweets inside the circle
+        center in gps location
+        radius given by user
+        '''
+        # TODO controllare che l'input sia numerico
+        lat = request.form['lat']
+        lon = request.form['lon']
+        radius = request.form['radius']
+        message = 'lat: ' + lat + ' lon: ' + lon + ' rad: ' + radius
+        if lat == '' or lon == '' or radius == '':
+            message = 'campo/i mancante'
+        return message
 
-# TODO: put this function in a class
-def init():
-    app.mongo = MongoClient()
-    app.db = app.mongo.test
-    app.run()
+    def export(self):
+        return "exporting"
+
 
 if __name__ == '__main__':
-	init()
- 
+
+    init()
