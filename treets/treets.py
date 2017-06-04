0eab5bb4d11e10874c362a2f0b27bf61dd1ccdde
dnlcrl <ciriello.daniele@gmail.com>
2017-06-04 02:13:24 +0200

    minor edits

--- treets/treets.py
+++ treets/treets.py
@@ -1,17 +1,21 @@
 from flask import Flask, render_template, request
-from pymongo import MongoClient
+from db_client import DBClient
 
 app = Flask(__name__)
 app.debug = True
 MAPBOX_ACCESS_KEY = 'pk.eyJ1Ijoibmljb2xhOTMiLCJhIjoiY2l2Y2ozYnZ5MDBocTJ5bzZiM284NGkyMiJ9.4VUvTxBv0zqgjY7t3JTFOQ'
+db_client = DBClient()
 
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
     return render_template('index.html', ACCESS_KEY=MAPBOX_ACCESS_KEY)
 
+
 def show_all_tweets():
-    import pdb; pdb.set_trace()
+    import pdb
+    pdb.set_trace()
+
 
 @app.route('/searchText', methods=['GET', 'POST'])
 def searchText():
@@ -32,11 +36,11 @@ def geo():
     center in gps location
     radius given by user
     '''
-    #TODO controllare che l'input sia numerico
+    # TODO controllare che l'input sia numerico
     lat = request.form['lat']
     lon = request.form['lon']
     radius = request.form['radius']
-    message = 'lat: '+ lat +' lon: '+ lon +' rad: '+ radius
+    message = 'lat: ' + lat + ' lon: ' + lon + ' rad: ' + radius
     if lat == '' or lon == '' or radius == '':
         message = 'campo/i mancante'
     return message
@@ -49,8 +53,6 @@ def export():
 
 # TODO: put this function in a class
 def init():
-    app.mongo = MongoClient()
-    app.db = app.mongo.test
     app.run()
 
 if __name__ == '__main__':
